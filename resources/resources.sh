0a704c18df42815b6906509b29f6d34c92af63b4
Jgitau <johngitau51@gmail.com>
2022-10-19 15:44:44 -0700

    Resources file added

---
+++ resources/resources.sh
@@ -0,0 +1,4 @@
+# Resources that are helpful in RNA-Seq analysis
+
+# Analyzing RNA-seq data with DESeq2
+http://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#standard-workflow1~http://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#standard-workflow1~http://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#standard-workflow1~http://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#standard-workflow1~http://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#funding
